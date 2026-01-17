7a51b7d6fa1c335a1a4a1073549b766c63e5de36
hardikagarwal23 <152130765+hardikagarwal23@users.noreply.github.com>
2026-01-17 11:30:33 +0530

    changing prompt value

--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -15,7 +15,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-  prompt: 'select_account'
+  prompt: 'cosent'
 });
 
 export { auth, provider, signInWithPopup };
